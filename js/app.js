a76ec59dc9c263f751dbd2c0d17e8e542da6ed36
Mishyn Aliaksei <trouvere777@mail.ru>
2020-04-25 21:14:34 +0300

    Add working modal panel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(function () {
+  /* Filter
+    =====================*/
   let filter = $("[data-filter]");
 
   filter.on("click", function (event) {
@@ -19,5 +21,64 @@ $(function () {
         }
       });
     }
+  });
+
+  /* Modal
+    =====================*/
+
+  const modalCall = $("[data-modal]");
+  const modalClose = $("[data-close]");
+
+  // Open
+  modalCall.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data("modal");
+
+    $(modalId).addClass("show");
+    $("body").addClass("no-scroll");
+
+    setTimeout(function () {
+      $(modalId).find(".modal__dialog").css({
+        transform: "scale(1)",
+      });
+    }, 200);
+  });
+
+  // Close
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents(".modal");
+
+    modalParent.find(".modal__dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  // Закрывает если нажато вне окна
+  $(".modal").on("click", function (event) {
+    let $this = $(this);
+
+    $this.find(".modal__dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  //Если нажато на окно - не закрывает
+  $(".modal__dialog").on("click", function (event) {
+    event.stopPropagation();
   });
 });
